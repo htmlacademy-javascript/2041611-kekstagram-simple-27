226815f1d765fa84f30dc65d75f983895d3bd0d3
Aleksey Yakovlev <volkracing555@gmail.com>
2022-10-02 13:39:13 +0300

    Нужно больше функций №2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 function randomNumber (min, max) {
-  if (min < 0 || max < 0 && max < min) {
+  if (min < 0 || max < 0 && max <= min && max === min) {
     return NaN;
   }
 
   // формула расчёта взята с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  return result;
 }
 
 randomNumber();
 
-
 function stringLength (string, maxLength) {
-  return string.length < maxLength;
+  const result = string.length <= maxLength;
+  return result;
 }
 
 stringLength();
