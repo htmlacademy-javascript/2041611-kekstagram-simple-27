8989840d1a1de95b3202bec8422cf654872c3938
Aleksey Yakovlev <volkracing555@gmail.com>
2022-10-15 21:01:45 +0300

    Отрисуй меня полностью

--- js/utils/util.js
+++ js/utils/util.js
@@ -1,4 +1,4 @@
-function randomNumber (min, max) {
+function getRandomPositiveInteger (min, max) {
   if (min < 0 || max < 0 && max <= min && max === min) {
     return NaN;
   }
@@ -11,11 +11,30 @@ function randomNumber (min, max) {
   return result;
 }
 
+//функцию-генератор для получения уникальных идентификаторов из указанного диапазона, и так, чтобы они не повторялись, пока не будут перебраны все числа из этого промежутка.
+function createRandomUniqueInteger (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
 // function stringLength (string, maxLength) {
 //   const result = string.length <= maxLength;
 //   return result;
 // }
 
-const getRandomArrayElement = (elements) => elements[randomNumber(0, elements.length - 1)];
+const generateRandomUniqueInteger = createRandomUniqueInteger(1, 25);
 
-export {randomNumber, getRandomArrayElement};
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+export {getRandomPositiveInteger, generateRandomUniqueInteger, getRandomArrayElement};
