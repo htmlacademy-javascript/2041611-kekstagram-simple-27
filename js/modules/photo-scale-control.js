4c73750676e9405d989186c25e512a858c8c868c
Aleksey Yakovlev <volkracing555@gmail.com>
2022-10-27 19:44:26 +0300

    Помощь друга

---
+++ js/modules/photo-scale-control.js
@@ -0,0 +1,46 @@
+const scaleCounter = document.querySelector('.scale__control--value');
+const scaleSmallerButton = document.querySelector('.scale__control--smaller');
+const scaleBiggerButton = document.querySelector('.scale__control--bigger');
+const photoPreview = document.querySelector('.img-upload__preview img');
+
+const MIN_SCALE_VALUE = 25;
+const MAX_SCALE_VALUE = 100;
+const DEFAULT_SCALE_STEP = 25;
+
+function setScaleOnPicture() {
+  const currentValue = parseFloat(scaleCounter.value);
+  photoPreview.style.transform = `scale(${currentValue / 100})`;
+}
+
+function onSmallerScaleClick() {
+  const currentValue = parseFloat(scaleCounter.value);
+
+  if (currentValue === MIN_SCALE_VALUE) {
+    return false;
+  }
+
+  scaleCounter.value = `${currentValue - DEFAULT_SCALE_STEP}%`;
+
+  setScaleOnPicture();
+}
+
+function onBiggerScaleClick() {
+  const currentValue = parseFloat(scaleCounter.value);
+
+  if (currentValue === MAX_SCALE_VALUE) {
+    return false;
+  }
+
+  scaleCounter.value = `${currentValue + DEFAULT_SCALE_STEP}%`;
+
+  setScaleOnPicture();
+}
+
+function resetScaleModifier() {
+  photoPreview.style.transform = '';
+}
+
+scaleSmallerButton.addEventListener('click', onSmallerScaleClick);
+scaleBiggerButton.addEventListener('click', onBiggerScaleClick);
+
+export {resetScaleModifier};
